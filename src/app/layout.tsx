f73291f392e9ab86a8d5106e4bd17c3203a6fbc9
Manuel De Ceglie <amoonpod@gmail.com>
2025-02-27 22:15:45 +0100

    speedinsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import type { Metadata } from "next";
 import { Bricolage_Grotesque as Bricolage_Grotesque } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 const fontSans = Bricolage_Grotesque({
   subsets: ["latin"],
   variable: "--font-sans",
@@ -103,6 +104,7 @@ export default function RootLayout({
           </TooltipProvider>
         </ThemeProvider>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
